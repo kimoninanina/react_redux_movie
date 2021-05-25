14835391f0a454c6e6dc73236d3cf1584fabc005
kimoninanina <nina02929@gmail.com>
2021-05-25 01:26:31 -0500

    Reusable List component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import MyList from './MyList';
+import MyList from './components/MyList';
+import Recommendations from './components/Recommendations';
+import logo from './logo.jpg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <MyList/>
+    <div className="app">
+      <img className="logo" alt="" src={logo} />
+      <MyList />
+      <Recommendations />
     </div>
   );
 }
